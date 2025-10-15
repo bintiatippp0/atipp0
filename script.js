7347d7b33cd3276945d3dbb68588e86a4f07ac6a
bintiatippp0 <bintinurlatiff11@gmail.com>
2025-10-15 21:27:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ window.addEventListener("scroll", reveal);
 
 // Menjalankan fungsi saat pertama kali load
 reveal();
+
+const scrollUpBtn = document.getElementById('scrollUpBtn');
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        scrollUpBtn.style.display = 'block';
+    } else {
+        scrollUpBtn.style.display = 'none';
+    }
+});
+scrollUpBtn.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
